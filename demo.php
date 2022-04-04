9075406c94ea23b7a9eb5a493351e7e610425a27
info@coders.VN <CodersTokyo>
2022-04-04 23:22:42 +0700

    add project

---
+++ demo.php
@@ -0,0 +1,13 @@
+﻿<?php
+    $courses = array();
+    $courses [] = "hao";
+    $courses[] = "new";
+
+    echo"<pre>";
+    print_r($courses);
+    echo"<pre>";
+
+    foreach($courses as $key => $value){
+        echo $value;
+    }
+?>
